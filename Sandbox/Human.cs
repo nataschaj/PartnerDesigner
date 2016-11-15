f365a40359711afe7b51f0bc11897accb9af11a4
Natascha Jakobsen <natascha_jakobsen@hotmail.com>
2016-11-15 13:12:02 +0100

    finished.

--- Sandbox/Human.cs
+++ Sandbox/Human.cs
@@ -7,58 +7,78 @@ namespace Sandbox
 {
     class Human
     {
-        private bool maleOrFemale;
-        private String eyeColor;
-        private String hairColor;
-        private int heightCategory;
+        //private bool maleOrFemale;
+        //private String eyeColor;
+        //private String hairColor;
+        //private int heightCategory;
+        public enum Gender { male, female }
+        public enum EyeColor { black, green, blue}
+        public enum HairColor { black, blond, white}
+        public enum Height { tall, medium, taller}
 
-        public Human(bool maleOrFemale, String eyeColor, String hairColor, int heightCategory)
+
+        public Gender genderEnum { get; set; }
+        public EyeColor eyeColorEnum { get; set; }
+        public HairColor hairColorEnum { get; set; }
+        public Height heightEnum { get; set; }
+
+        //constructor
+        public Human(Gender pgender, EyeColor peyeColor, HairColor phairColor, Height pheight)
         {
-            this.maleOrFemale = maleOrFemale;
-            this.eyeColor = eyeColor;
-            this.hairColor = hairColor;
-            this.heightCategory = heightCategory;
+            this.genderEnum = pgender;
+            this.eyeColorEnum = peyeColor;
+            this.hairColorEnum = phairColor;
+            this.heightEnum = pheight;
         }
+
+        //public Human(bool maleOrFemale String eyeColor, String hairColor, int heightCategory)
+        //{
+        //    this.maleOrFemale = maleOrFemale;
+        //    this.eyeColor = eyeColor;
+        //    this.hairColor = hairColor;
+        //    this.heightCategory = heightCategory;
+        //}
+
 
         public String GetDescription()
         {
-            String description = "You got a " + GetGenderDescription();
-            description = description + ", with " + eyeColor + " eyes";
-            description = description + ", " + hairColor + " hair";
-            description = description + ", who is " + GetHeightDescription();
+            String description = "You got a " + genderEnum;
+            description = description + ", with " + eyeColorEnum + " eyes";
+            description = description + ", " + hairColorEnum + " hair";
+            description = description + ", who is " + heightEnum;
 
             return description;
         }
 
-        public String GetGenderDescription()
-        {
-            if (maleOrFemale)
-            {
-                return "man";
-            }
-            else
-            {
-                return "woman";
-            }
-        }
+        //public String GetGenderDescription()
+        //{
+        //    if (maleOrFemale)
+        //    {
+        //        return "man";
+        //    }
+        //    else
+        //    {
+        //        return "woman";
+        //    }
+        //}
 
-        public String GetHeightDescription()
-        {
-            switch (heightCategory)
-            {
-                case 0:
-                    return "Very short";
-                case 1:
-                    return "Short";
-                case 2:
-                    return "Medium height";
-                case 3:
-                    return "Tall";
-                case 4:
-                    return "Very tall";
-                default:
-                    return "Unknown height";
-            }
-        }
+        //public String GetHeightDescription()
+        //{
+        //    switch (heightCategory)
+        //    {
+        //        case 0:
+        //            return "Very short";
+        //        case 1:
+        //            return "Short";
+        //        case 2:
+        //            return "Medium height";
+        //        case 3:
+        //            return "Tall";
+        //        case 4:
+        //            return "Very tall";
+        //        default:
+        //            return "Unknown height";
+        //    }
+        //}
     }
 }
--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -8,13 +8,13 @@ namespace Sandbox
         {
             // The FIRST line of code should be BELOW this line
 
-            Human h1 = new Human(true, "Brown", "Black", 1);
-            Human h2 = new Human(false, "White", "Blue", 4);
-            Human h3 = new Human(true, "Blond", "Green", 8);
+            Human h1 = new Human(Human.Gender.female, Human.EyeColor.black, Human.HairColor.black, Human.Height.tall);
+            //Human h2 = new Human(false, "White", "Blue", 4);
+            //Human h3 = new Human(true, "Blond", "Green", 8);
 
             Console.WriteLine(h1.GetDescription());
-            Console.WriteLine(h2.GetDescription());
-            Console.WriteLine(h3.GetDescription());
+            //Console.WriteLine(h2.GetDescription());
+            //Console.WriteLine(h3.GetDescription());
 
 
             // The LAST line of code should be ABOVE this line
